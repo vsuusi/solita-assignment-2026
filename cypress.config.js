132c800605de21cd6ba728a1b7bfe170bf8c9216
Santeri Uusitalo <vs.uusitalo@gmail.com>
2026-01-18 18:58:58 +0200

    enabled cypress in CI

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173",
+    baseUrl: "http://localhost:8080",
     setupNodeEvents(on, config) {},
   },
 });
